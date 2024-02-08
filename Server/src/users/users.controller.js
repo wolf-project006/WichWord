924dd0fdc615fea650b6d4d351a40a1b4ff0faab
RH-Kevin <147596439+RH-Kevin@users.noreply.github.com>
2024-02-08 17:01:35 +0900

    add: MVC arch + post ep

--- Server/src/users/users.controller.js
+++ Server/src/users/users.controller.js
@@ -1,11 +1,33 @@
 const usersModel = require("./users.model");
 
 
+
+// API endpoint logic goes in this file. 
+
 module.exports = {
     async index(req, res) {
         const users = await usersModel.getAllUsers();
         //res.render("" , { users });
         console.log(users);
         res.status(200).send(users);
+    },
+
+    async newUser(req, res) {
+        const {
+            user_name,
+            nick_name,
+        } = req.body;
+
+        const payload = {
+            user_name: user_name,
+            nick_name: nick_name,
+        };
+
+        let user;
+
+        // Implement "does user already exist?" later
+
+        user = await usersModel.addNewUser(payload);
+        res.status(200).send(user);
     }
 }
--- Server/src/users/users.model.js
+++ Server/src/users/users.model.js
@@ -2,10 +2,11 @@ const knex = require("../../knex");
 
 
 
+// Knex queries go in this file
 
+
+// "users" is the name of the table in the local DB
 const USERS_TABLE = "users";
-
-
 
 module.exports = {
     USERS_TABLE,
@@ -15,5 +16,11 @@ module.exports = {
     getAllUsers(limit = 5) {
         return knex
         .select("*").from(USERS_TABLE).limit(limit);
+    },
+
+    // "user" is the payload seen in controller
+    addNewUser(user) {
+        return knex.insert(user).into(USERS_TABLE);
+
     }
 }
