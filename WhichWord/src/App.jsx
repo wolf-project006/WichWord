f2e40fb95e34b3de6ec9a8189b9b66bb60c51537
yurikahirata <yurikahirata0130@gmail.com>
2024-02-08 15:50:12 +0900

    feat: validation of player answer, final score

--- WhichWord/src/App.jsx
+++ WhichWord/src/App.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import MainMenu from './components/MainMenu'
-import BaseQuestion from './components/BaseQuestion'
+import { useState, useEffect } from 'react';
+import './App.css';
+import MainMenu from './components/MainMenu';
+import BaseQuestion from './components/BaseQuestion';
+import ScoreScreen from './components/ScoreScreen';
 
 function App() {
 
-  const [view, setView] = useState("MainMenu")
+  const [view, setView] = useState("MainMenu"); // Dictates what component is rendered
+  const [score, setScore] = useState(0);
+  const [viewHTML, setViewHTML] = useState(<></>); // What is rendered
+
+  // Everytime {view} changes, html component stored changes
+  useEffect(() => {
+    if (view === "MainMenu")
+      setViewHTML(<MainMenu setView={setView} />);
+    else if (view === "BaseQuestion")
+      setViewHTML(<BaseQuestion setScore={setScore} setView={setView} />);
+    else
+      setViewHTML(<ScoreScreen score={score} setScore={setScore} setView={setView} />);
+  }, [view])
 
   return (
     <>
-      {(view === "MainMenu") ?
-        <MainMenu setView={setView} /> :
-        <BaseQuestion />
-      }
+      {viewHTML}
     </>
   )
 }
--- WhichWord/src/components/BaseQuestion.jsx
+++ WhichWord/src/components/BaseQuestion.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import '../App.css';
 
-const BaseQuestion = () => {
+const BaseQuestion = ({ setScore, setView }) => {
 
-  const [answer, setAnswer] = useState("");
+  const [answer, setAnswer] = useState(""); // Player answer
   const [heandAndTail, setHeadAndTail] = useState([]);
 
   const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 
+  // Set head and tail letters
   useEffect(() => {
     const head = ALPHABET[Math.floor(Math.random() * ALPHABET.length)];
     const tail = ALPHABET[Math.floor(Math.random() * ALPHABET.length)];
@@ -16,15 +17,25 @@ const BaseQuestion = () => {
     setHeadAndTail(newArr);
   }, []);
 
-  function handleOnClick() {
+  async function handleOnClick() {
+    const answerToFetch = (heandAndTail[0] + answer + heandAndTail[1]).toLowerCase();
+    const result = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${answerToFetch}`);
+    const parsedResult = await result.json();
 
+    // If word exists
+    if (parsedResult[0] && parsedResult[0]["word"]) {
+      const points = answer.length;
+      setScore(points);
+    }
+
+    setView("ScoreScreen");
   }
 
   return (
     <>
       <div>
         <h1 className="letter">{heandAndTail[0]}</h1>
-        <input type="text" onChange={e => setAnswer(e.target.value)} />
+        <input type="text" onChange={(e) => setAnswer(e.target.value)} />
         <h1 className="letter">{heandAndTail[1]}</h1>
       </div>
       <button onClick={handleOnClick} value={answer} >Submit</button>
