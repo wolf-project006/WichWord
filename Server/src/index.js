41f87fc5974f590ca2eb78d5b329023e31ffcdb4
RH-Kevin <147596439+RH-Kevin@users.noreply.github.com>
2024-02-09 21:00:10 +0900

    feat: password hashing

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,7 +2,7 @@ const {setupServer} = require("./server.js");
 require('dotenv').config({path: '../.env'});
 
 const server = setupServer();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
     console.log("Server listening on Port: ", PORT);
