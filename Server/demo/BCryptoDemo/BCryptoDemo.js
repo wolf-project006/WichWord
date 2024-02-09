325216da308ce9ab3c1d3b6b69354760c2527820
jiaxiangu <gujiaxian1997@gmail.com>
2024-02-09 14:03:52 +0900

    chore: add comment to bcrypt demo code

--- Server/demo/BCryptoDemo/BCryptoDemo.js
+++ Server/demo/BCryptoDemo/BCryptoDemo.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const bcrypt = require('bcrypt'); // need to install bcrypt: npm install bcrypt
+const saltRounds = 10;  // this variable is used to make the salt. Bcrypt handles the salt things for us.
 
+// We only need to store hashed password in db. No need to store salt any more.
 
 app.use(express.json());
 app.use(cors());
@@ -19,6 +20,7 @@ const PlainPassword = "987654321gujiaxian";
 let hashedPassword = "";
 
 app.get("/password", async(req, res) => {
+    //below are the code to make hashed password.
     bcrypt.genSalt(saltRounds, function(err, salt) {
         bcrypt.hash(PlainPassword, salt, function(err, hash) {
             console.log(hash);
@@ -28,7 +30,10 @@ app.get("/password", async(req, res) => {
     });
 })
 
+
 app.get("/password/compare", async(req, res) => {
+    // below are function used to check if the password is correct when user login.
+    // compare the PlainPassword, which is the password user input, with hashedPassword.
     bcrypt.compare(PlainPassword, hashedPassword, function(err, result) {
         console.log(hashedPassword)
         res.status(200).send(result);
