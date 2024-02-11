b17c86a51d5a6b5cd3cbbd613a207a41f62c26af
yurikahirata <yurikahirata0130@gmail.com>
2024-02-11 23:21:48 +0900

    style: MainMenu, BaseQuestion, and ScoreScreen colors

--- WhichWord/src/App.jsx
+++ WhichWord/src/App.jsx
@@ -10,13 +10,14 @@ function App() {
   const [viewHTML, setViewHTML] = useState(<></>); // What is rendered
 
   const [score, setScore] = useState(0);
-  const [personalBest, setPersonalBest] = useState(0);
+  // const [username, setUsername] = useState("");
+  //const [personalBest, setPersonalBest] = useState(0);
 
-  // Fetching user's personal
+  // Fetching user data --> move to after login/create account screen
   // useEffect(() => {
   //   const getPersonalBest = await fetch("/highscore/username");
   //   setPersonalBest(getPersonalBest);
-  // })
+  // }, [])
 
   const components = {
     MainMenu: () => { return <MainMenu setView={setView} /> },
