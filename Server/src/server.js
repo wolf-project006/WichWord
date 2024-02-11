08a229a44c2ef2650ab038171ab8596bcccc6df5
RH-Kevin <147596439+RH-Kevin@users.noreply.github.com>
2024-02-11 13:38:55 +0900

    implement: basic auth (test)

--- Server/src/server.js
+++ Server/src/server.js
@@ -3,9 +3,10 @@ const knex = require('../knex');
 const app = express();
 const cors = require('cors');
 
-// BCrypt Imports
+// Security / BCrypt 
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const session = require('express-session');
 
 // Controllers 
 const usersController = require("./users/users.controller");
@@ -13,14 +14,29 @@ const usersController = require("./users/users.controller");
 const setupServer = () => {
     app.use(express.json());
     app.use(cors());
-    
+
+    // Express Session
+    app.use(session({
+        secret: 'your_secret_key', // A secret key for signing the session ID cookie.
+        resave: false, // Do not force the session to be saved back to the session store.
+        saveUninitialized: false, // Do not force a session that is "uninitialized" to be saved to the store.
+        cookie: { secure: true, maxAge: 60000 } // Cookie settings, `secure: true` should be used for HTTPS.
+      }));
+
+    const users = [
+        {
+            id: 1,
+            user_name: '',
+            passwordHash: ''
+        },
+    ];
 
     app.get("/", (req, res) => {
         res.status(200).send("I am up and running!");
     });
 
 
-    
+
 
 
 
@@ -33,6 +49,7 @@ const setupServer = () => {
     // User Routes
     app.get("/users", usersController.index);
     app.post("/users", usersController.newUser);
+    app.post("/login", usersController.login);
 
     return app;
 }
