718c84cf108a90e30b60ae2474da70762adea224
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-15 10:31:24 +0900

    feat: quickstart button

--- WhichWord/src/components/MainMenu.jsx
+++ WhichWord/src/components/MainMenu.jsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React from 'react';
 import '../App.css';
 
 const MainMenu = ({ setView }) => {
-
   return (
     <>
       <h1 className="letter">W</h1>
       <h1 className="letter orange orange-border">ichWor</h1>
-      <h1 className="letter">d</h1 >
+      <h1 className="letter">d</h1>
       <div>
-        <button className="marginTop_btn" onClick={() => { setView("Login") }}>Login</button>
+        <button
+          className="marginTop_btn"
+          onClick={() => {
+            setView('BaseQuestion1');
+          }}
+        >
+          Quick-start
+        </button>
       </div>
       <div>
-        <button className="marginTop_btn" onClick={() => { setView("CreateAccount") }}>Signup</button>
+        <button
+          className="marginTop_btn"
+          onClick={() => {
+            setView('Login');
+          }}
+        >
+          Login
+        </button>
+      </div>
+      <div>
+        <button
+          className="marginTop_btn"
+          onClick={() => {
+            setView('CreateAccount');
+          }}
+        >
+          Signup
+        </button>
       </div>
     </>
   );
-}
+};
 
 export default MainMenu;
